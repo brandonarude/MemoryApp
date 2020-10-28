399f19821de7d479edf81ffbd5a1d9624b8a0a0a
BrandonRude <brandon@rude.family>
2020-10-28 14:26:50 -0700

    incrementDayCounter function is starting to work

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import {
   TouchableOpacity,
   Text,
   View,
+  Alert,
 } from 'react-native'
 import {findDate} from './src/Components/Atoms/findDate.js'
+import {incrementDayCounter} from './src/Components/Atoms/incrementDayCounter.js'
 
 function RetrievePassage(props){
   return(
@@ -21,9 +23,11 @@ class Hello extends Component {
   render() {
     let thing = 5000000;
     let fullDate = findDate();
+    let newDay = incrementDayCounter(fullDate);
     return (
       <View>
-        <RetrievePassage day={fullDate[1]} />
+
+        <RetrievePassage day={newDay} />
       </View>
 
     )
