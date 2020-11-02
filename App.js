a6b4321901eda130c56e5bd0059c09877f3e9dee
BrandonRude <brandon@rude.family>
2020-11-02 13:48:49 -0800

    Working Component view in another file (home.js)

--- App.js
+++ App.js
@@ -1,61 +1,19 @@
-import 'react-native-gesture-handler';
-import React, { useState, useEffect, Component } from 'react'
-import {
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  View,
-  Alert
-} from 'react-native'
-import {findDate} from './src/Components/Atoms/findDate.js'
-import {incrementDayCounter} from './src/Components/Atoms/incrementDayCounter.js'
-import {retrieveStoredDate} from './src/Components/Atoms/retrieveStoredDate.js'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {save} from './src/Components/Atoms/saveAsyncData.js'
-import {LinearGradient} from 'expo-linear-gradient';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {HomeScreen} from "./src/Navigations/home.js"
 
-class Hello extends Component {
-constructor(props){
-  super(props);
-  this.state = {
-    date: "",
-    saved: ""
-  };
+const Stack = createStackNavigator();
 
-  let dataToSave = "Test Again";
-
-  save(dataToSave);
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-componentDidMount() {
-    let loadedDate = retrieveStoredDate();
-    loadedDate.then(loadedDate => this.setState({date: loadedDate}));
-}
-
-render(){
-    return(
-      <LinearGradient start={[.6,0]} colors={['#d500ff', '#6306aa', '#340979']} style={styles.container}>
-      <Text style={styles.title}>
-      {this.state.date}
-      </Text>
-      </LinearGradient>
-    )
-  }
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    padding: 24,
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin:20,
-    borderRadius: 5,
-  },
-  title: {
-    fontSize: 30,
-  }
-});
-
-export default Hello;
+export default App;
