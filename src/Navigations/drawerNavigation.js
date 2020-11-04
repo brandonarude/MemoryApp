95f2a16db3f6336c2c5a3fb3986635bf15c73c2e
BrandonRude <brandon@rude.family>
2020-11-04 14:27:49 -0800

    Added Basic view for powerOfGodsWord.js and added to Drawer

--- src/Navigations/drawerNavigation.js
+++ src/Navigations/drawerNavigation.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import DailyReadingTabs from './dailyReadingTabs';
 import LanguageSelection from './Screens/languageSelect.js'
+import PowerOfGodsWord from './Screens/powerOfGodsWord.js'
 
 const Drawer = createDrawerNavigator();
 
@@ -11,6 +12,7 @@ export default function AppDrawer() {
       <Drawer.Navigator>
         <Drawer.Screen name="Today's Reading" component={DailyReadingTabs} />
         <Drawer.Screen name="Language Selection" component={LanguageSelection} />
+        <Drawer.Screen name="Power of God's Word" component={PowerOfGodsWord} />
       </Drawer.Navigator>
   );
 }
