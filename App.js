77d0d522c1bf7c3dac3284dd8dc15046e7114418
BrandonRude <brandon@rude.family>
2020-11-04 14:03:41 -0800

    Deleted Unneccesary imports in App.js

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
 import * as React from 'react';
-import { useState, Component } from 'react';
-import { Button, View, Text, TextInput, Easing, Image, ImageBackground, TouchableOpacity, SafeAreaView } from 'react-native';
+import { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, TransitionPresets, CardStyleInterpolators } from '@react-navigation/stack';
-import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import styles from './src/Styles/styles.js';
-import DailyEncouragement from './src/Navigations/Screens/dailyEncouragement.js';
-import DailyReadingTabs from './src/Navigations/dailyReadingTabs.js'
+import { createStackNavigator } from '@react-navigation/stack';
 import AppDrawer from './src/Navigations/drawerNavigation.js'
 
 const Stack = createStackNavigator();
