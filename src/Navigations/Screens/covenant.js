076e5eb19e68c10f70fb9ca95aa64aff0ab10977
BrandonRude <brandon@rude.family>
2020-11-04 15:17:35 -0800

    Created Translation Helper for Drawer Titles

--- src/Navigations/Screens/covenant.js
+++ src/Navigations/Screens/covenant.js
@@ -1,4 +1,18 @@
 import * as React from 'react';
-import {  View, Text, TouchableOpacity } from 'react-native';
+import {  View, Text, TouchableOpacity, Image } from 'react-native';
 import { Component } from 'react';
 import styles from '../../Styles/styles.js';
+import images from '../../Assets/Images/imageLibrary.js';
+
+export default function Covenant({navigation}) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.drawerHamburgerContainer} onPress={() => navigation.openDrawer()}>
+        <Image source={images.hamburgerButtonImage} style={styles.drawerHamburgerImage}/>
+      </TouchableOpacity>
+      <Text>
+        Covenant Goes Here.
+      </Text>
+    </View>
+  )
+}
--- src/Navigations/drawerNavigation.js
+++ src/Navigations/drawerNavigation.js
@@ -2,17 +2,35 @@ import * as React from 'react';
 import { Component } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import DailyReadingTabs from './dailyReadingTabs';
-import LanguageSelection from './Screens/languageSelect.js'
-import PowerOfGodsWord from './Screens/powerOfGodsWord.js'
+import LanguageSelection from './Screens/languageSelect.js';
+import PowerOfGodsWord from './Screens/powerOfGodsWord.js';
+import TranslationsUsed from './Screens/translationsUsed.js';
+import KeyWords from './Screens/keyWordDescription.js';
+import FiveVoicesInfo from './Screens/fiveVoicesInfo.js';
+import Covenant from './Screens/covenant.js';
+import LocalizeDrawerTitles from '../Localization/drawerTranslations.js';
 
-const Drawer = createDrawerNavigator();
+let Drawer = createDrawerNavigator();
+
+let titles = LocalizeDrawerTitles();
 
 export default function AppDrawer() {
   return (
       <Drawer.Navigator>
-        <Drawer.Screen name="Today's Reading" component={DailyReadingTabs} />
-        <Drawer.Screen name="Language Selection" component={LanguageSelection} />
-        <Drawer.Screen name="Power of God's Word" component={PowerOfGodsWord} />
+        <Drawer.Screen name="Today's Reading" component={DailyReadingTabs}
+          options={{title: titles.dailyReadingTitle}} />
+        <Drawer.Screen name="Language Selection" component={LanguageSelection}
+          options={{title: titles.languageSelectionTitle}} />
+        <Drawer.Screen name="Power of God's Word" component={PowerOfGodsWord}
+          options={{title: titles.powerOfGodsWordTitle}} />
+        <Drawer.Screen name="What are the Five Voices?" component={FiveVoicesInfo}
+          options={{title: titles.fiveVoicesInfoTitle}} />
+        <Drawer.Screen name="Memory Covenant" component={Covenant}
+          options={{title: titles.covenantTitle}} />
+        <Drawer.Screen name="Glossary" component={KeyWords}
+          options={{title: titles.keyWordInfoTitle}} />
+        <Drawer.Screen name="Translations Used" component={TranslationsUsed}
+          options={{title: titles.translationsUsedTitle}} />
       </Drawer.Navigator>
   );
 }
